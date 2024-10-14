529583d641a85c1d853e7c9c3445b4ed987c8504
lng205 <lng205.yb@gmail.com>
2024-10-14 21:51:08 +0800

    删去用于实时更新ice状态的WebSocket部分代码，因为寻找更优p2p路径非本项目核心内容。

--- server.py
+++ server.py
@@ -1,31 +1,18 @@
-import asyncio
 from aiohttp import web
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from track import SyntheticVideoStreamTrack as Track
-import json
-
-pcs = set()
 
 
 async def index(request):
     return web.FileResponse("index.html")
 
 
-async def offer(request):
+async def offer(request: web.Request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
     pc = RTCPeerConnection()
-    pcs.add(pc)
 
-    @pc.on("iceconnectionstatechange")
-    async def on_iceconnectionstatechange():
-        print("ICE connection state is %s" % pc.iceConnectionState)
-        if pc.iceConnectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
-
-    # Use the synthetic video track as the video source
     video_track = Track()
     pc.addTrack(video_track)
 
@@ -39,47 +26,8 @@ async def offer(request):
     )
 
 
-async def websocket(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    pc = RTCPeerConnection()
-    pcs.add(pc)
-
-    async for msg in ws:
-        if msg.type == web.WSMsgType.TEXT:
-            data = json.loads(msg.data)
-            if "sdp" in data:
-                offer = RTCSessionDescription(sdp=data["sdp"], type=data["type"])
-                await pc.setRemoteDescription(offer)
-                answer = await pc.createAnswer()
-                await pc.setLocalDescription(answer)
-                await ws.send_json(
-                    {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-                )
-
-            elif "candidate" in data:
-                candidate = data["candidate"]
-                if candidate:
-                    await pc.addIceCandidate(candidate)
-
-        elif msg.type == web.WSMsgType.ERROR:
-            print(f"WebSocket connection closed with exception {ws.exception()}")
-
-    return ws
-
-
-async def cleanup():
-    # Close peer connections
-    coros = [pc.close() for pc in pcs]
-    await asyncio.gather(*coros)
-    pcs.clear()
-
-
 app = web.Application()
-app.add_routes(
-    [web.post("/offer", offer), web.get("/ws", websocket), web.get("/", index)]
-)
+app.add_routes([web.post("/offer", offer), web.get("/", index)])
 
 if __name__ == "__main__":
     web.run_app(app, port=8080)
